fb0907cec1407e362930f13ec37b55f47ec029f2
MD R Islam <mdislamwork@gmail.com>
2022-07-23 22:25:21 -0400

    form

--- trail-ui/src/App.js
+++ trail-ui/src/App.js
@@ -5,7 +5,7 @@ import {
   TeamOutlined,
   UserOutlined
 } from '@ant-design/icons';
-import { Breadcrumb, Layout, Menu, Input, Card, Space, Col, Row, Button, Divider } from 'antd';
+import { Breadcrumb, Layout, Menu, Input, Card, Space, Col, Row, Button, Divider, Skeleton, Form } from 'antd';
 import React, { useState } from 'react';
 const { Search } = Input;
 const { Header, Content, Footer, Sider } = Layout;
@@ -13,12 +13,14 @@ const axios = require('axios');
 const { Meta } = Card;
 
 const dweb = (cid) => `https://${cid}.ipfs.dweb.link/`;
-const onSearch = async (uniqueCode, setFoundData) => {
+const onSearch = async (uniqueCode, setFoundData, setLookupCode) => {
+  setLookupCode(true);
   let results = await listUploads();
   console.log(results.data);
   let files = await Promise.all(results.data.map(upload => axios.get(dweb(upload.cid))));
   console.log(files.map(resp => resp.data));
   let filtered = files.filter(resp => resp.data.device_pinValue.replaceAll(" ", "") === uniqueCode.replaceAll(" ", ""));
+  setLookupCode(false);
   if (filtered.length == 1) {
     console.log("filtered", filtered[0].data);
     setFoundData(filtered[0].data);
@@ -27,7 +29,7 @@ const onSearch = async (uniqueCode, setFoundData) => {
 
 const getNftImageUrl = async () => {
   const perPage = 10;
-  const pageNumber = Math.floor(Math.random() * 10); 
+  const pageNumber = Math.floor(Math.random() * 10);
   const selection = Math.floor(Math.random() * 9);
   const imageResp = await axios
     .get(`https://api.pexels.com/v1/search?query=mountain&page=${pageNumber}&per_page=${perPage}`,
@@ -47,7 +49,7 @@ const listAllNftsOwned = async () => {
   });
 }
 
-const pollAndInflateNftCard = async (hash, setMintedCard) => {
+const pollAndInflateNftCard = async (hash, setMintedCard, setMinting) => {
   console.log("begin polling for", hash);
   const mineUrl = `https://api.nftport.xyz/v0/mints/${hash}?chain=rinkeby`
   axios.get(mineUrl, {
@@ -62,11 +64,12 @@ const pollAndInflateNftCard = async (hash, setMintedCard) => {
       console.log("minted", minted);
       if (minted.length == 1) {
         if (minted[0]["file_url"]) {
+          setMinting(false);
           setMintedCard(minted[0]);
         }
         else {
           console.log("file_url missing, trying again");
-          setTimeout(() => pollAndInflateNftCard(hash, setMintedCard), 3000);
+          setTimeout(() => pollAndInflateNftCard(hash, setMintedCard, setMinting), 3000);
         }
       } else {
         console.error("minted was not 1", minted);
@@ -74,12 +77,13 @@ const pollAndInflateNftCard = async (hash, setMintedCard) => {
     })
     .catch(function (error) {
       console.log("axios err, will retry", error);
-      setTimeout(() => pollAndInflateNftCard(hash, setMintedCard), 5000);
+      setTimeout(() => pollAndInflateNftCard(hash, setMintedCard, setMinting), 5000);
     });
 
 }
 
-const mintNtf = async (uniqueCode, setMintHidden, setMintedCard) => {
+const mintNtf = async (uniqueCode, setMintHidden, setMintedCard, setMinting) => {
+  setMinting(true);
   setMintHidden(true);
   console.log("mint with", uniqueCode)
   const mintUrl = "https://api.nftport.xyz/v0/mints/easy/urls";
@@ -98,7 +102,7 @@ const mintNtf = async (uniqueCode, setMintHidden, setMintedCard) => {
     }
   });
 
-  setTimeout(() => pollAndInflateNftCard(mintResp.data["transaction_hash"], setMintedCard), 5000);
+  setTimeout(() => pollAndInflateNftCard(mintResp.data["transaction_hash"], setMintedCard, setMinting), 5000);
 };
 
 // 122 222 34
@@ -118,15 +122,82 @@ function createNftCard(nftData) {
     <div>
       <br />
       <Card
-      hoverable
-      style={{ width: 370 }}
-      cover={<img alt="example" src={nftData["file_url"]} />}
-    >
-      <Meta title={nftData["name"]} description={nftData["description"]} />
-    </Card>
+        hoverable
+        style={{ width: 720 }}
+        cover={<img alt="example" src={nftData["file_url"]} />}
+      >
+        <Meta title={nftData["name"]} description={nftData["description"]} />
+      </Card>
     </div>
   );
 };
+
+const createForm = () => {
+  const onFinish = (values) => {
+    console.log('Success:', values);
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Difficulty"
+        name="difficulty"
+        rules={[
+          {
+            required: false,
+          }
+        ]}
+      >
+        <Input.TextArea />
+        <span>How difficult was this hike?</span>
+      </Form.Item>
+
+      <Form.Item
+        label="Road Condition"
+        name="condition"
+        rules={[
+          {
+            required: false
+          }
+        ]}
+      >
+        <Input.TextArea />
+        <span>How was the road/trail condition? Any obstructions?</span>
+      </Form.Item>
+
+      <Form.Item
+        label="Rating"
+        name="rating"
+        rules={[
+          {
+            required: false
+          }
+        ]}
+      >
+        <Input />
+        <span>On a scale of 1-10, how enjoyable was this hike?</span>
+      </Form.Item>
+    </Form>
+  );
+}
 
 function getItem(label, key, icon, children) {
   return {
@@ -147,6 +218,8 @@ const App = () => {
   const [foundData, setFoundData] = useState(null);
   const [mintHidden, setMintHidden] = useState(false);
   const [mintedCard, setMintedCard] = useState(null);
+  const [lookupCode, setLookupCode] = useState(false);
+  const [minting, setMinting] = useState(false);
 
   return (
     <Layout
@@ -187,12 +260,22 @@ const App = () => {
           >
             <Row>
               <Col span={6}>
-                <Search placeholder="input search text" onSearch={v => onSearch(v, setFoundData)} enterButton />
+                <Search placeholder="input search text" onSearch={v => onSearch(v, setFoundData, setLookupCode)} enterButton />
+              </Col>
+            </Row>
+            <Row>
+              <Col span={24}>
               </Col>
             </Row>
 
             <Row>
-              <Col span={6} offset={8}>
+              <Col span={12} offset={6}>
+                {lookupCode && (
+                  <div>
+                    <br />
+                    <Skeleton active />
+                  </div>)
+                }
                 {foundData && (
                   <div>
                     <br />
@@ -201,19 +284,26 @@ const App = () => {
                     >
                       {`Created: ${foundData["device_dateCreated"]}`}
                       <Divider />
-                      <Button disabled={mintHidden} type="primary" shape="round" size={"large"} onClick={() => mintNtf(foundData["device_pinValue"], setMintHidden, setMintedCard)}>
+                      {createForm()}
+                      <Button disabled={mintHidden} type="primary" shape="round" size={"large"} onClick={() => mintNtf(foundData["device_pinValue"], setMintHidden, setMintedCard, setMinting)}>
                         Mint!
                       </Button>
                     </Card>
                   </div>)}
               </Col>
             </Row>
-              <Row>
-                <br />
-                <Col span={24} offset={8}>
-                  {mintedCard && createNftCard(mintedCard)}
-                </Col>
-              </Row>
+            <Row>
+              <br />
+              <Col span={6} offset={8}>
+                {minting && (<div>
+                  <br />
+                  <Skeleton active />
+                </div>)}
+              </Col>
+              <Col span={24} offset={6}>
+                {mintedCard && createNftCard(mintedCard)}
+              </Col>
+            </Row>
           </div>
         </Content>
         <Footer
